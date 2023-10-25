117c3b35f7500fda5c6d655e347b12bec5245d9e
buptLiYangCode <1642612105@qq.com>
2023-10-25 22:06:17 +0800

    新增员工代码

--- sky-pojo/src/main/java/com/sky/dto/EmployeeDTO.java
+++ sky-pojo/src/main/java/com/sky/dto/EmployeeDTO.java
@@ -4,10 +4,14 @@ import lombok.Data;
 
 import java.io.Serializable;
 
+/**
+ * DTO主要用于在不同层之间传递数据。
+ * VO主要用于在视图层中展示数据。
+ * Entity则是用于在数据库中存储数据的对象。
+ */
 @Data
 public class EmployeeDTO implements Serializable {
 
-    private Long id;
 
     private String username;
 
